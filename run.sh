500d6f5cef9a061ef2b12edd467afba99939f043
yongshanding <yongshanding@gmail.com>
2018-08-14 17:56:43 -0500

    modular exponentiation

---
+++ run.sh
@@ -0,0 +1,9 @@
+FILE=mult
+cd build/
+make -j 12 >/dev/null 2>/dev/null 
+cd -  
+rm -rf ${FILE}*  
+rm -rf scripts/rev-memory-manager-hybrid.bcpp  
+echo "[run.sh] ${FILE} begins..."
+./scripts/gen-rev-mem-optimized.sh Algorithms/${FILE}.scaffold >${FILE}.err 2> ${FILE}.err2
+tail ${FILE}/*freq > ${FILE}.out 
