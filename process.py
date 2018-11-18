034ef1221d8106e6a0bb9c63a3a7118c1e1e2b38
yongshanding <yongshanding@gmail.com>
2018-11-17 21:44:35 -0600

    first test on exhaust search works

--- process.py
+++ process.py
@@ -2,24 +2,28 @@
 
 lines = []
 vols = []
-with open("results.processed", "r") as f:
+with open("first.results.out", "r") as f:
     lines = f.readlines()
     linesplits = [x.split() for x in lines]
-    print(len(linesplits))
-    for x in range(255):
-        qbs = int(linesplits[3*x][-1][:-1])
-        ts = int(linesplits[3*x+1][-1][:-1])
-        vol = qbs * ts
+    #print(len(linesplits))
+    for x in range(1023):
+        qbs = int(linesplits[10*x+1][-1][:-1])
+        ts = int(linesplits[10*x+3][-1][:-1])
+        vol = (qbs, ts)
         vols.append(vol)
-
+min_q = 0
+min_g = 0
 min_vol = 10000000
 min_idx = -1
 
 for x in range(len(vols)):
-    if vols[x] < min_vol:
-        min_vol = vols[x]
+    vol = vols[x][0]*vols[x][1]
+    if (vol < min_vol):
+        min_vol = vol
         min_idx = x
+        min_q = vols[x][0]
+        min_g = vols[x][1]
 
-print (format(x,'08b'))
-print (min_vol)
+print (str(min_idx)+": "+str(format(min_idx,'010b')))
+print ("Min vol: " + str(min_q) + " x " + str(min_g) + " = " + str(min_vol))
 
--- run_loop.sh
+++ run_loop.sh
@@ -1,13 +1,18 @@
-FILE=best_multiplier
+FILE=first
+n=10
+N=1024
 cd build/
-make -j 12 #>/dev/null 2>/dev/null 
+make -j 12 >/dev/null 2>/dev/null 
 cd -  
 rm -rf ${FILE}*  
 rm -rf scripts/rev-memory-manager-hybrid.bcpp  
 echo "[run.sh] ${FILE} begins..."
 touch ${FILE}.results.out
-for i in $(seq 1 256) ; do
-	./scripts/gen-rev-mem-optimized.sh Algorithms/${FILE}.scaffold #>${FILE}.err 2> ${FILE}.err2
+echo "[run.sh] Generating on_off_sequences.txt of ${n} bits..."
+python gen-all-seqs.py -n ${n}
+echo "[run.sh] Running ${N} iterations..."
+for i in $(seq 1 ${N}) ; do
+	./scripts/gen-rev-mem-optimized.sh Algorithms/${FILE}.scaffold >>${FILE}.err 2>> ${FILE}.err2
 	tail -n +2 on_off_sequences.txt > seq.tmp && mv seq.tmp on_off_sequences.txt	
 	tail "${FILE}/${FILE}.flat000k.freq" >> "${FILE}.results${i}.out"
 done
